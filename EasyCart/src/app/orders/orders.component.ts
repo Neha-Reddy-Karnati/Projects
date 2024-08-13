c6d768b57881b54b16062df114a9c3f6276dbaa7
Neha Reddy Karnati <nehakarnati2001@gmail.com>
2024-08-13 11:47:30 -0400

    Add files via upload

--- EasyCart/src/app/orders/orders.component.ts
+++ EasyCart/src/app/orders/orders.component.ts
@@ -1,1 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CusServiceService } from '../cus-service.service';
+import { Router } from '@angular/router';
+var jQuery:any;
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+customer:any;
+custId:any;
+orders:any;
+ordDetails:any;
+ord:any;
 
+  constructor(private service :CusServiceService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.customer =  JSON.parse(sessionStorage.getItem('customer'));
+    this.custId = this.customer.custId;
+    this.service.getOrdersByCustomerId(this.custId).subscribe((result: any) => { console.log(result); this.orders = result; });
+  
+  }
+
+  
+  details(ord){
+    this.service.getOrderDetailsByOrderId(ord.orderId).subscribe((result: any) => { console.log(result); this.ordDetails = result; });
+    jQuery('#orderDetailsModel').modal('show');
+
+  }
+
+
+}
